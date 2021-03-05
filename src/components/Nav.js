7f7ecb528ab6f9b5addfc0d1aa698103ed3397de
Adam Allen <aallen6@travelers.com>
2021-03-05 13:12:28 -0500

    cleaned up code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,6 @@ import {
 } from 'react-router-dom'
 
 const Nav = (props) => {
-  // if (location.pathname.startsWith('/search')) {
-  //   const query = location.pathname.substring(8)
-  //   props.performSearch(query)
-  // }
-
   const categories = props.categories.map((category, index) =>
     <li key={index}>
       <NavLink to={`/search/${category}`}>{category}</NavLink>
